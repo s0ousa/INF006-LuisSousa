4d0716c173da94b7feccdeb58755d0f33670a63f
Luiss <70407063+s0ousa@users.noreply.github.com>
2022-09-05 21:28:57 +0000

    respondndo questoes

--- RevisaoGeralLingC/questao10.c
+++ RevisaoGeralLingC/questao10.c
@@ -3,24 +3,28 @@
 impares. */
 
 #include <stdio.h> 
-#define tam 10
+#define tam 6
 void main() {
-int matriz[tam][tam];
-int l ;
-int c ;
+  int matriz[tam][tam];
+  int l ;
+  int c ;
 
-for(l = 0; l <tam ; l++) {
-    
-  for ( c = 0; c <tam; c++) {
-        printf("Digite o valor da linha %i e coluna %i  \n", l , c);
-        scanf("%i", &matriz[l][c]);
-
+  for(l = 0; l <tam ; l++) {
+      
+    for ( c = 0; c <tam; c++) {
+          printf("Digite o valor da linha %i e coluna %i  \n", l , c);
+          scanf("%i", &matriz[l][c]);
+  
+    }   
   }
 
-  int i = 0;
-  int j = 0;
-  for(i; i<tam; i+2,j+2) {
-  printf("%i", matriz[l][c]);
-}
-}
+
+  printf("Saida: \n");
+  int i = 1 ;
+  int j= 0 ;
+  for (i = 1; i < tam; i+=2 ) {
+    printf("%i \n", matriz[i][j]);
+    j+=2;
   }
+}
+
--- RevisaoGeralLingC/questao11.c
+++ RevisaoGeralLingC/questao11.c
@@ -1,1 +1,42 @@
 /* Criar e armazenar ua matriz a 10x10 e gerar e imprimir a matriz invertida */
+
+#include <stdio.h> 
+#define tam 4
+void main() {
+  int matriz[tam][tam];
+  int invertida[tam][tam];
+  int l ;
+  int c ;
+
+  for(l = 0; l <tam ; l++) {
+      
+    for ( c = 0; c <tam; c++) {
+          printf("Digite o valor da linha %i e coluna %i  \n", l , c);
+          scanf("%i", &matriz[l][c]);
+  
+    }   
+  }
+
+  int auxi = tam-1;
+  
+  
+  for(int i = 0; i<tam ; i++) {
+    int auxj = tam-1;
+    for(int j = 0 ; j<tam ; j++, auxj--) {
+      invertida[i][j] = matriz[auxi][auxj];
+    }
+    
+    auxi--;
+  }
+
+  l = 0;
+  c = 0;
+  for(l ; l <tam ; l++) {
+    printf ("\n ");
+    for ( c ; c <tam; c++) {
+          printf("%i ", invertida[l][c]);
+
+    }   
+  }
+  
+}
