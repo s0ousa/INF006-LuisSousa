b815ee6ab3f4cf3a169ac026c1f4bd26932e9041
Luis <las.sousa@outlook.com>
2022-09-22 17:10:29 -0300

    continuandoquestoes

---
+++ continuacaoRevisao/questao5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#define TAM 5 
+
+//5) Que leia os elemntos de uma matriz 10x10 
+//e mostre somente os elementos abaixo da diagonal;
+
+
+void main() {
+
+/* int matriz[5][5] = {{3,2,5,7,8},
+                    {1,6,2,9,5},
+                    {8,4,1,0,4},
+                    {2,7,5,4,9},
+                    {12,4,2,5,7}};
+*/
+    int matriz[TAM][TAM];
+
+    for(int l = 0; l <TAM; l++) {
+        for(int c = 0; c <TAM; c++) {
+            printf("Digite o valor da linha %i, coluna %i\n", l,c);
+            scanf("%i", &matriz[l][c]);
+        }
+    }  
+
+    int limiteColuna = 1;
+    for(int l = 1; l<TAM;l++) {
+        if(limiteColuna<=TAM-1) {
+            for(int c = 0; c<limiteColuna;c++) {
+                printf("%i ", matriz[l][c]);
+             }
+       
+            limiteColuna++;
+        }
+        
+
+        printf("\n");
+    }
+
+}
--- continuacaoRevisao/questao7.c
+++ continuacaoRevisao/questao7.c
@@ -1,11 +1,60 @@
 #include <stdio.h>
 #define TAM 5
 
-//Que declare uma matriz 5x5  e armazene em um vetor o maior elemento cadastrado em cada coluna da matriz, 
+//Que declare uma matriz 5x5  e armazene em um vetor o maior elemento cadastrado em cada coluna da matriz,
 //e em um vetor b o menor elemento cadastrado em cada coluna da matriz e imprima:
 // a) toda a matrix; b) o vetor a; c) o vetor b; d)(a=b)/2
 
+void printaVetor(int vetorAtual[TAM]) {
+    for(int i = 0; i <TAM; i ++) {
+            printf("%d ", vetorAtual[i]);
+        }
+    printf("\n");  
+
+}
 
 void main() {
+    int matriz[5][5] = {{3,2,5,7,8},
+                        {1,6,2,9,5},
+                        {8,4,1,0,4},
+                        {2,7,5,4,9},
+                        {12,4,2,5,7}};
+    int vetorMaiores[5];
+    int vetorMenores[5];
+   
+    int l = 0;
+    int c = 0;
 
+
+    int menorAtual;
+    int maiorAtual;
+
+
+     while(c <5) {
+        l = 0;
+        menorAtual = matriz[l][c];
+        maiorAtual = matriz[l][c];
+        
+        for(l; l<5;l++) {
+        
+            if (matriz[l][c]<menorAtual) {
+            menorAtual = matriz[l][c];
+            }
+
+            if (matriz[l][c]>maiorAtual) {
+            maiorAtual = matriz[l][c];
+        }
+        }
+    
+
+        vetorMaiores[c] = maiorAtual;
+        vetorMenores[c] = menorAtual;
+
+        c++;
+    }
+
+    puts("Vetor maiores :");
+    printaVetor(vetorMaiores);
+    puts("Vetor menores :");
+    printaVetor(vetorMenores);
 }
